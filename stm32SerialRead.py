cbf8786f0fa2ec7b7ae67a5b3276c75217a8f165
pikkela <simon.hiltunen@gmail.com>
2021-12-02 21:00:05 +0200

    Communication changed to tx and rx pins
    	-serial communication changed from usb to tx and rx pins
    	-changed string presentation with unpack to 3 doubles

--- stm32SerialRead.py
+++ stm32SerialRead.py
@@ -2,6 +2,7 @@ import serial
 import string
 import time
 import sqlite3
+import struct
 
 def get_db_connection():
         conn = sqlite3.connect('database.db')
@@ -9,15 +10,20 @@ def get_db_connection():
         return conn
 
 #open serial port
-ser = serial.Serial('/dev/ttyACM0',9600)
+ser = serial.Serial(port='/dev/ttyS0',baudrate=9600,parity=serial.PARITY_NONE,stopbits=1,bytesize=8,timeout=None)
 #check ls /dev/tty* for ttyUSBx port
 conn = get_db_connection()
 while True :
-	serialdata = ser.readline()
-	data = serialdata.split(" ")
+	serialdata = ser.read(24)
+	#serialData = ser.readline()
+	#print(serialdata)
+	x,y,z = struct.unpack('ddd',serialdata)
+	print "\n\nx = ",x,"\ny = ",y, "\nz = ",z
+	#print("\n\nx = " + x + "\ny = " + y + "\nz = " + z)
+	#data = serialdata.split(" ")
         #BME280 temperature 20 C
         #INSERT INTO logs (id, title, content)
-	conn.execute('INSERT INTO logs (devId, title, content) VALUES (?, ?, ?)',
-             	     (data[0].decode('utf-8'), data[1].decode('utf-8'), data[2].decode('utf-8')+data[3].decode('utf-8')))
-	conn.commit()
+	#conn.execute('INSERT INTO logs (devId, title, content) VALUES (?, ?, ?)',
+        #     	     (data[0].decode('utf-8'), data[1].decode('utf-8'), data[2].decode('utf-8')+data[3].decode('utf-8')))
+	#conn.commit()
 conn.close()
